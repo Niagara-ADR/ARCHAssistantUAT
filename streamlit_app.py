dab497f00ab479116de2c8ca496c2ebb273238b0
Niagara-ADR <adr_niagara_test@outlook.com>
2024-08-13 15:40:01 +0000

    Many edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,69 +6,51 @@ import pandas as pd
 import json
 import time
 from dateutil.relativedelta import relativedelta
-import re
 import pytz
-
-  
+from typing_extensions import override
+from openai import AssistantEventHandler
+    
 st.title("ARCH UAT Ticket Assistant")
 
 openai_api_key = st.secrets.openai_api_key
 
 client = OpenAI(api_key=openai_api_key)
 
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+
 class AssistantManager:
     assistant_id = st.secrets.assistant_id
 
     def __init__(self):
         self.client = client
-        self.model = "gpt-4o"
+        self.model = "gpt-3.5-turbo"
         self.thread = None
         self.run = None
         self.assistant = self.client.beta.assistants.retrieve(
             assistant_id=AssistantManager.assistant_id
         )
 
-    def get_ticket_details(self, start_date=None, end_date=None, date_string=None):
-        if not start_date and not end_date:
-            if not date_string:
-                date_string = "recent"
-            today = datetime.today()
-            if date_string == "past month":
-                start_date = today - timedelta(days=30)
-                end_date = today
-            elif date_string == "last year":
-                start_date = today.replace(year=today.year - 1, month=1, day=1)
-                end_date = today.replace(year=today.year - 1, month=12, day=31)
-            elif date_string == "this year" or date_string == "past year":
-                start_date = today.replace(month=1, day=1)
-                end_date = today
-            elif date_string == "recent" or date_string == "past week" or date_string == "this year":
-                start_date = today - timedelta(days=7)
-                end_date = today
-            else:
-                match_days = re.match(r'last (\d+) days', date_string)
-                match_weeks = re.match(r'last (\d+) weeks', date_string)
-                match_months = re.match(r'last (\d+) months', date_string)
-                
-                if match_days:
-                    days = int(match_days.group(1))
-                    start_date = today - timedelta(days=days)
-                    end_date = today
-                elif match_weeks:
-                    weeks = int(match_weeks.group(1))
-                    start_date = today - timedelta(weeks=weeks)
-                    end_date = today
-                elif match_months:
-                    months = int(match_months.group(1))
-                    start_date = today - relativedelta(months=months)
-                    end_date = today
-                else:
-                    start_date = today - timedelta(days=7)
-                    end_date = today
-            start_date = start_date.strftime('%Y-%m-%d')
-            end_date = end_date.strftime('%Y-%m-%d')
-
-
+    def get_ticket_details(self):
         url = st.secrets.token_url
         payload = {
             "grant_type": "client_credentials",
@@ -84,13 +66,12 @@ class AssistantManager:
                 'Authorization': 'Bearer:' + access_token,
                 'Content-Type': 'application/json'
             }
-        if not start_date or datetime.strptime(start_date, "%Y-%m-%d").date() > datetime.now().date():
-            start_date = (datetime.now().date() - timedelta(days=7)).strftime('%Y-%m-%d')
-        if not end_date or datetime.strptime(end_date, "%Y-%m-%d").date() > datetime.now().date():
-            end_date=(datetime.now().date()).strftime('%Y-%m-%d')
+        
+        start_date = (datetime.now().date() - timedelta(weeks=1000)).strftime('%Y-%m-%d')
+        end_date=(datetime.now().date()).strftime('%Y-%m-%d')
         print(start_date)
         print(end_date)
-        response = requests.get('https://uat-archapi.niagarawater.com/api/tickets/getAllTicketDetails?usecase_id=["60cb784aafe4530011138ca9"]&start_date={}&end_date={}'.format(start_date, end_date), headers = headers)
+        response = requests.get('https://archapi.niagarawater.com/api/tickets/getAllTicketDetails?usecase_id=["60cb784aafe4530011138ca9"]&start_date={}&end_date={}'.format(start_date, end_date), headers = headers)
         json = response.json()
         df = pd.DataFrame(json['response'])
         df_plants = pd.read_excel('Plant Acronyms.xlsx')
@@ -130,12 +111,31 @@ class AssistantManager:
         except:
             pass
         print(df)
-        return df.to_json(orient='index')
+        df.to_excel('temp.xlsx', index=False)
+        file = client.files.create(
+            file=open("temp.xlsx", "rb"),
+            purpose='assistants'
+        )
+        thread = client.beta.threads.create(
+            messages=[
+                {
+                "role": "user",
+                "content": "Answer questions about tickets using the file.",
+                "attachments": [
+                    {
+                    "file_id": file.id,
+                    "tools": [{"type": "code_interpreter"}]
+                    }
+                ]
+                }
+            ]
+        )
+        return thread
 
     
     def create_thread(self):
         if not self.thread:
-            thread_obj = self.client.beta.threads.create()
+            thread_obj = self.get_ticket_details()
             self.thread = thread_obj
             print(f"CREATED NEW THREAD: {self.thread.id}")
 
@@ -161,8 +161,15 @@ class AssistantManager:
             response = last_message.content[0].text.value
             summary.append(response)
 
-            self.summary = "\n".join(summary)
-            return response
+        with client.beta.threads.runs.stream(
+            thread_id=self.thread.id,
+            assistant_id=AssistantManager.assistant_id,
+            event_handler=EventHandler(),
+        ) as stream:
+            stream.until_done()
+
+        self.summary = "\n".join(summary)
+        return response
 
     def call_required_functions(self, required_actions):
         if not self.run:
